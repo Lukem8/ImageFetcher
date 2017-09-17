9643d82d44a399c3fcf884b151b6a6ae7ee3e4ea
Luke Moore <lmoore7@g.ucla.edu>
2017-09-17 11:58:20 -0700

    download process working

--- imgdownload.py
+++ imgdownload.py
@@ -2,19 +2,31 @@
 import os
 import sys
 import urllib2
-from BeautifulSoup import BeautifulSoup as bs
 from urllib import urlretrieve
 import urlparse
+import json
+import re
+
+
+pattern_image = re.compile("\.\w+$")
+
+def download(i, data, directory):
+    filename = "image" + str(i)
+    outpath = os.path.join(directory, filename)
+    f = open(directory + "image"+ str(i)+".jpg", 'wb')
+    img = urllib2.urlopen(data["url"])
+    f.write(img.read())
+
 
 def main():
     print "This program will download 20 pictures from r/wallpaper and save to the images directory\n"
-    
+
+    image_extensions = ["jpg", "jpeg", "png"]
+
     # create directory to save images to if it does not already exist
     directory = "images/"
     if not os.path.exists(directory):
         os.makedirs(directory)
-    ## download the page and parse html document, find your image with regex and download it
-    ## use urllib2 for downloading and Beautiful Soup for parsing html file.
 
     # Set custom User-Agent to allow request to be sent
     headers = { 'User-Agent' : 'Mozilla/5.0' }
@@ -23,14 +35,19 @@ def main():
     req = urllib2.Request('https://www.reddit.com/r/wallpaper/', None, headers)
     html = urllib2.urlopen(req).read()
 
-    soup = bs(urllib2.urlopen(req))
+    response = urllib2.urlopen('http://www.reddit.com/r/wallpaper' + '/hot/.json?limit=' + '50')    # download 20 images from "hot" category
+    data = json.load(response)
 
-    i = 0
-    for image in soup.findAll("img"):
-        print "Image: %(src)s" % image
-        filename = "image" + str(i)
-        outpath = os.path.join(directory, filename)
-        i = i + 1
+    i = 1
+    for element in data["data"]["children"]:
+        if pattern_image.search(element["data"]["url"]):
+            if element["data"]["url"].split(".")[-1] in image_extensions:
+                print "Downloading image " + str(i) + "..."
+                download(i, element["data"], directory)
+                i += 1
+        if i > 20:
+            break
+    print "Download complete."
 
 if __name__ == "__main__":
     main()
